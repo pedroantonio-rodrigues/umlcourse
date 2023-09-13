2ad253cba6bdb41f9a81fd4aaafd736330938759
Pedro Antonio <pedro_arxp@hotmail.com>
2023-09-13 20:43:42 -0300

    Creating a new class Category and testing it

--- src/main/java/com/pedro/umlcourse/umlcourse/resource/CategoryResource.java
+++ src/main/java/com/pedro/umlcourse/umlcourse/resource/CategoryResource.java
@@ -1,8 +1,13 @@
 package com.pedro.umlcourse.umlcourse.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro.umlcourse.umlcourse.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -10,6 +15,14 @@ public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
     public String listar(){
+
+        Category cat1 = new Category(1, "Informatica");
+        Category cat2 = new Category(2, "Escritorio");
+
+         List<Category> list = new ArrayList<>();   
+
+         list.add(cat1);
+         list.add(cat2);
 
         return  "Rest are working";
     }
