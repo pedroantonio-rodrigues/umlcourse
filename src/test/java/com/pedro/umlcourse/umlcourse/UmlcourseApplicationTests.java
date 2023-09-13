8f6045333542b981ce6e25fd258d4ca945c181dd
Pedro Antonio <pedro_arxp@hotmail.com>
2023-09-13 20:05:51 -0300

    start a new project wirh spring

---
+++ src/test/java/com/pedro/umlcourse/umlcourse/UmlcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.umlcourse.umlcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
